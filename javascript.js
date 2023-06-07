8b5294cee20da3b87bac12a6c0d7f13b529ec393
Aman Ullah Khan <amanullahkhan91929@gmail>
2023-06-07 12:44:58 +0500

    javascript (prompt)

---
+++ javascript.js
@@ -0,0 +1,32 @@
+
+
+// var userRegistered = "Aman";
+// var userInput = prompt("What is your name?");
+// console.log(userRegistered === userInput);
+
+
+var userRegistered = "Aman";
+var userInput = prompt("What is your name?");
+
+if (userRegistered === userInput) {
+    console.log("Wellcome sir " + userRegistered);
+}
+
+else {
+    console.log("Sir please Register First.");
+}
+
+
+
+
+// // PROMPT PTACTICE:
+
+// var Marks = 90;
+// var result = +prompt("Please enter your marks");
+
+// if(result >= Marks){
+//     console.log('I will buy you a heavy bike.');
+// }
+// else{
+//     console.log('please study more for good result.');
+// }
